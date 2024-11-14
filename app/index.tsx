72a6a7a0429a2c0c465106ced955dafdb28018b9
ahmadnadeem6236 <ahmadnadeem6236@gmail.com>
2024-11-14 15:59:18 +0530

    HomeScree completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,42 @@
-import { StyleSheet, View, Text } from "react-native";
-import "../global.css";
-export default function HomeScreen() {
+import { View, Text, ImageBackground } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+
+export default function App() {
   return (
-    <View style={styles.titleContainer}>
-      <Text className=" text-2xl">Hey Hello World Nadeem</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          style={{ flex: 1 }}
+          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+        >
+          <SafeAreaView className="flex-1 justify-between mx-5 my-12">
+            <View>
+              <Text className="text-white font-bold text-4xl text-center tracking-wider">
+                SUKOON
+              </Text>
+              <Text className="text-white font-normal text-2xl text-center tracking-wider">
+                Sukoon for Everyone
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                onPress={() => console.log("text")}
+                title="Get Started"
+              />
+            </View>
+          </SafeAreaView>
+        </LinearGradient>
+        <StatusBar style="light" />
+      </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./app/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
+    "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
+    "./app/(tabs)/meditate.tsx",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rmono: ["Roboto-Mono", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
